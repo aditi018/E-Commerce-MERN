f7f14487addf14c3ee89b3a98c93c52dd77d70f3
aditi018 <dasaditi018@gmail.com>
2022-12-16 22:36:02 +0530

    updated user route

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -12,6 +12,27 @@ module.exports = (err,req,res,next)=>{
         err = new ErrorHandler(message,400);
     }
 
+
+    //Mongoose duplicate key error
+    if(err.code === 11000){
+        const message = `Duplicate ${Object.keys(err.keyValue)} entered `
+        err = new ErrorHandler(message,400)
+    }
+
+    //Wrong JWT Error
+    if(err.code === "JsonWebTokenError"){
+        const message = "JSON Web Token is Invalid.Try Again";
+
+        err = new ErrorHandler(message , 400)
+    }
+
+    //JWT Expire Error
+    if(err.code === "TokenExpiredError"){
+        const message = "Json Web Token is already expired";
+
+        err = new ErrorHandler(message,400);
+    }
+
     res.status(err.statusCode).json({
         success:false,
         message:err.message
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,15 +3,25 @@ const {
     registerUser,
     loginUser,logout, 
     forgotPassword,
-    resetPassword
+    resetPassword,
+    getUserDetails,
+    updatePassword,
+    updateUserProfile
 } = require("../controllers/userController");
+
+const {isAuthenticatedUser} = require("../middleware/auth");
 
 
 router.route("/register").post(registerUser);
 
 router.route("/login").post(loginUser);
-router.route("/password/forgot").post(forgotPassword);
+router.route("/password/update").post(forgotPassword);
 router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
+router.route("/getUser").get(isAuthenticatedUser,getUserDetails);
+
+router.route("/password/update").put(isAuthenticatedUser,updatePassword)
+
+router.route("/update/profile").put(isAuthenticatedUser,updateUserProfile);
 
 module.exports = router;
